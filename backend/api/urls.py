854b0a0e5b1ea4a1428da208b1ff1b7c898708fd
Sandy Fuentes <sfuent1@gmail.com>
2024-06-21 10:30:38 -0400

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,11 @@
 # api/urls.py
 
 from django.urls import path
-from .views import get_competitions, get_competitors, get_matches, get_competitor_matches
+from .views import CompetitionList, CompetitorList, MatchList, CompetitorMatchList
 
 urlpatterns = [
-    path('competitions/', get_competitions, name='get_competitions'),
-    path('competitions/<competition_id>/competitors/', get_competitors, name='get_competitors'),
-    path('competitions/<competition_id>/matches/', get_matches, name='get_matches'),
-    path('competitions/<competition_id>/competitors/<competitor_id>/matches/', get_competitor_matches, name='get_competitor_matches'),
+    path('competitions/', CompetitionList.as_view(), name='get_competitions'),
+    path('competitions/<competition_id>/competitors/', CompetitorList.as_view(), name='get_competitors'),
+    path('competitions/<competition_id>/matches/', MatchList.as_view(), name='get_matches'),
+    path('competitions/<competition_id>/competitors/<competitor_id>/matches/', CompetitorMatchList.as_view(), name='get_competitor_matches'),
 ]
